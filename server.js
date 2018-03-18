7f61475630a488171dd37371fab5418908dd7aeb
Giovanni Pardo <giovanni_pardo@utexas.edu>
2018-03-18 15:34:39 -0500

    Adding javascript and styling

--- server.js
+++ server.js
@@ -1,198 +1,61 @@
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-var path = require("path");
-
-// requiring note and article models 
-var Note = require ("./models/Note.js");
-var Article = require ("./models/Article.js");
-
-// scraping tools
-var request = require("request");
-var cheerio = require("cheerio");
-
+var exphbs = require("express-handlebars");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
-//define port
-var port = process.env.PORT || 3000 
-
-//initilaize express
+// Initialize Express
 var app = express();
 
-//using morgan and body parser
-app.use(logger("dev"));
+// Use body parser with our app
 app.use(bodyParser.urlencoded({
-    extended: false
+   extended: false
 }));
 
-// make public static dir
-app.use(express.static("public"));
+// Make public a static dir
+app.use(express.static(process.cwd() + "/public"));
 
-// set handlebars
-var exphbs = require("express-handlebars");
+// Database configuration with mongoose
+var databaseUri = "mongodb://localhost/mongoosearticles";
 
-app.engine("handlebars" , exphbs({
-    defaultLayout: "main",
-    partialsDir: path.join(__dirname, "/views/layouts/partials")
-}));
-app.set("view engine", "handlebars");
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
 
-
-// database config with mongoose - need to finish
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds215019.mlab.com:15019/heroku_dw8gbmd0");
 var db = mongoose.connection;
 
-//show mongoose errors
-db.on("error", function(error){
-    console.log("Mongoose error:", error);
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
 });
 
-//log success message for mongoose
-db.once("open", function(){
-    console.log("Mongoose connection successful.");
+db.once("open", function() {
+  console.log("Mongoose connection sucessful.");
 });
 
-//Routes
+//set engine and default for handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Get requests
-app.get("/", function(req,res){
-    Article.find({"saved": false}, function(error, data){
-        var hbsObject = {
-            article: data
-        };
-        console.log(hbsObject);
-        res.render("home", hbsObject);
-    });
-});
+// Import routes and give the server access to them.
+var router = express.Router();
 
-app.get("/saved", function(req, res){
-    Article.find({"saved":true}).populate("notes").exec(function(error, articles){
-        var hbsObject = {
-            article: articles
-        };
-        res.render("saved", hbsObject);
-    });
-});
+// Require routes file pass router object
+require("./config/routes")(router);
 
-app.get("/scrape", function(req, res) {
-request("https://nytimes.com/", function(error, response, html){
-    var $ = cheerio.load(html);
-    $("article").each(function(i, element){
-        var result = {};
+// Have every request go through router middlewar
+app.use(router);
 
-        result.title = $(this).children("h2").text();
-        result.summary = $(this).children(".summary").text();
-        result.link = $(this).children("h2").children("a").attr("href");
+//set port
+var port = process.env.PORT || 3001;
 
-        var entry = new Article(result);
-        entry.save(function(err, doc){
-            if(err){
-                console.log(err);
-            } else {
-                console.log(doc);
-            }
-        });
-
-    });
-    res.send("scrape complete");
-});
-});
-
-// getting articles scraped from mongodb
-app.get("/articles", function(req,res){
-    Article.find({}, function(error, doc){
-        if (error){
-            console.log(error);
-        } else {
-            res.json(doc);
-        }
-    });
-});
-
-// getting articles by it's objectid
-app.get("/articles/:id", function(req, res){
-    Article.findOne({"_id": req.params.id})
-    .populate("note")
-    .exec(function(error, doc){
-        if(error){
-            console.log(error);
-        } else {
-            res.json(doc);
-        }
-    });
-});
-
-// delete an article
-app.post("/articles/delete/:id", function(req, res){
-    Article.findOneAndUpdate({ "_id": req.params.id}, { " saved": true})
-    .exec(function(err, doc){
-        if (err){
-            console.log(err);
-        } else {
-            res.send(doc);
-        }
-    });
-});
-
-app.post("/articles/delete/:id", function(req, res){
-    Article.findOneAndUpdate({ "_id": req.params.id}, {"saved": false, "notes":[]})
-    .exec(function(err,doc){
-        if (err){
-            console.log(err);
-
-        }else {
-            res.send(doc);
-        }
-    });
-});
-
-//creating a new note
-app.post("/notes/save/:id", function (req, res){
-    var newNote = new Note ({
-        body: req.body.text,
-        article: req.params.id
-    });
-    console.log(req.body)
-    newNote.save(function(error, note){
-        if (error){
-            console.log(error);
-        } else {
-            Article.findOneAndUpdate({ "_id": req.params.id}, {$push: { "notes": note}} )
-            .exec(function(err){
-                if(err){
-                    console.log(err);
-                    res.send(err);
-                } else {
-                    res.send(note);
-                }
-            });
-
-        }
-    });
-});
-
-// deleting a note
-
-app.delete("/notes/delete/:note_id/:article_id", function(req, res){
-    Note.findOneAndUpdate({ "_id": req.params.note_id}, function(err){
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            Article.findOneAndUpdate({ "_id": req.params.id}, {$pull: {"notes": req.params.note_id}})
-            .exec(function(err){
-                if (err){
-                    console.log(err);
-                    res.send(err);
-                } else {
-                    res.send("Note Deleted");
-                }
-            });
-        }
-    });
-});
-
-app.listen(port, function(){
-    console.log("App running on port " + port);
+//setup listener
+app.listen(port, function() {
+  console.log("app running on port " + port);
 });
